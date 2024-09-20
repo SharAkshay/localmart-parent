dc86e3eebdae426268c296d4718de62702968bc0
asharma <akshay.sharma@dataart.com>
2024-09-20 13:55:58 +0300

    Initial commit with basic classes

---
+++ src/main/java/com/localmart/localmartparent/LocalmartParentApplication.java
@@ -0,0 +1,13 @@
+package com.localmart.localmartparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalmartParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalmartParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/localmart/localmartparent/LocalmartParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localmart.localmartparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalmartParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
